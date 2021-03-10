f75689190f2533f779692332f0c9aa04b691e922
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-10 23:23:16 +0200

    add register component

--- src/app/submodules/public/submodules/user/components/register/register.component.ts
+++ src/app/submodules/public/submodules/user/components/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+	AbstractControl,
+	FormBuilder,
+	FormGroup,
+	ValidationErrors,
+	ValidatorFn,
+	Validators,
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '@app/root/reducers';
 import { Router } from '@angular/router';
@@ -8,6 +15,8 @@ import { userRegister } from '@app/public/submodules/user/actions/user-register.
 import { Observable } from 'rxjs';
 import { selectUserIdentity } from '@app/public/submodules/user/selectors/user.selectors';
 import { mapIsLoading, mapIsSuccess } from '@app/root/observable.helpers';
+import { childrenEqual } from '@app/custom-validators';
+import { toastSuccess } from '@app/root/actions/toast.actions';
 
 @Component({
 	selector: 'app-register',
@@ -17,6 +26,7 @@ import { mapIsLoading, mapIsSuccess } from '@app/root/observable.helpers';
 export class RegisterComponent implements OnInit {
 	form: FormGroup;
 	hidePassword = true;
+	hideConfirmPassword = true;
 
 	private registerLoading$ = new Observable<boolean>();
 	private registerSuccess$ = new Observable<boolean>();
@@ -37,11 +47,15 @@ export class RegisterComponent implements OnInit {
 		this.form = this.fb.group({
 			username: ['', Validators.compose([Validators.required])],
 			password: ['', Validators.compose([Validators.required])],
+			confirmPassword: ['', Validators.compose([Validators.required])],
 		});
 
 		this.registerSuccess$.subscribe((ok) => {
 			if (ok) {
 				this.router.navigate(['/private']);
+				this.store.dispatch(
+					toastSuccess({ message: 'Successfully registered' })
+				);
 			}
 		});
 	}
@@ -57,13 +71,38 @@ export class RegisterComponent implements OnInit {
 					password: this.form.get('password')?.value,
 				})
 			);
-		} else {
-			console.log(this.form.get('username')?.errors);
-			console.log(this.form.get('password')?.errors);
 		}
 	}
 
 	isLoading(): Observable<boolean> {
 		return this.registerLoading$;
+	}
+
+	get password(): AbstractControl | null {
+		return this.form.get('password');
+	}
+	get confirmPassword(): AbstractControl | null {
+		return this.form.get('confirmPassword');
+	}
+
+	onPasswordInput() {
+		let pass = this.form.get('password');
+		let confirmPass = this.form.get('confirmPassword');
+		if (pass && confirmPass) {
+			if (pass.value !== confirmPass.value) {
+				confirmPass.setErrors([{ passwordMismatch: true }]);
+			} else {
+				confirmPass.setErrors(null);
+			}
+		}
+	}
+
+	toggleHidePassword($event: MouseEvent) {
+		this.hidePassword = !this.hidePassword;
+		$event.stopPropagation();
+	}
+	toggleHideConfirmPassword($event: MouseEvent) {
+		this.hideConfirmPassword = !this.hideConfirmPassword;
+		$event.stopPropagation();
 	}
 }
