f2ae558a968b8db523753871aa682b1e8fd21f85
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-10 23:21:40 +0200

    add toast actions and effects

--- src/app/actions/toast.actions.ts
+++ src/app/actions/toast.actions.ts
@@ -1,9 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 
-export const loadToasts = createAction(
-  '[Toast] Load Toasts'
-);
+export const TOAST_ERROR = '[Toast] Error toast';
+export const TOAST_SUCCESS = '[Toast] Success toast';
 
-
-
-
+export const toastError = createAction(TOAST_ERROR, props<any>());
+export const toastSuccess = createAction(TOAST_SUCCESS, props<any>());
