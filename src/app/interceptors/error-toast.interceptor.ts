ef5b2b430c85725fde51e7625fd098632a230eff
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-10 23:19:04 +0200

    add interceptors

--- src/app/interceptors/error-toast.interceptor.ts
+++ src/app/interceptors/error-toast.interceptor.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
 import {
-	HttpErrorResponse,
 	HttpEvent,
 	HttpHandler,
 	HttpInterceptor,
 	HttpRequest,
 } from '@angular/common/http';
-import { ToastrService } from 'ngx-toastr';
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { ServerError } from '@app/shared/models/server-error';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from '@env/environment';
+import { ToastService } from '@app/shared/services/toast.service';
+import { AppState } from '@app/root/reducers';
+import { Store } from '@ngrx/store';
+import { toastError } from '@app/root/actions/toast.actions';
 
 @Injectable()
 export class ErrorToastInterceptor implements HttpInterceptor {
-	constructor(private toasterService: ToastrService) {}
+	constructor(private store: Store<AppState>) {}
 
+	openSnackBar(err: any) {
+		if (environment.production) {
+			return;
+		}
+		this.store.dispatch(toastError(err));
+	}
 	intercept(
 		req: HttpRequest<any>,
 		next: HttpHandler
 	): Observable<HttpEvent<any>> {
-		return next.handle(req);
+		return next.handle(req).pipe(
+			tap(
+				() => {},
+				(err: any) => {
+					this.openSnackBar(err);
+				}
+			)
+		);
 	}
 }
---
+++ src/app/interceptors/unauthorized.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+	HttpErrorResponse,
+	HttpEvent,
+	HttpHandler,
+	HttpInterceptor,
+	HttpRequest,
+} from '@angular/common/http';
+import { userLogout } from '@app/public/submodules/user/actions/user-logout.actions';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '@app/root/reducers';
+
+@Injectable()
+export class UnauthorizedInterceptor implements HttpInterceptor {
+	constructor(private store: Store<AppState>) {}
+
+	intercept(
+		request: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		return next.handle(request).pipe(
+			tap(
+				() => {},
+				(err: any) => {
+					if (err instanceof HttpErrorResponse) {
+						if (err.status === 401) {
+							this.store.dispatch(userLogout());
+						}
+					}
+				}
+			)
+		);
+	}
+}
