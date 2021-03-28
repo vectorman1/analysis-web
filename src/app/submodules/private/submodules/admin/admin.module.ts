222cc5284702a188831f322b8fad9f4428e5f1b6
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-29 01:32:27 +0300

    update imports of admin module

--- src/app/submodules/private/submodules/admin/admin.module.ts
+++ src/app/submodules/private/submodules/admin/admin.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-
+import { AdminComponent } from './admin.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AdminBaseComponent } from './components/admin-base/admin-base.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+	declarations: [AdminComponent, AdminBaseComponent],
+	imports: [
+		CommonModule,
+		AdminRoutingModule,
+		FlexLayoutModule,
+		MatCardModule,
+		MatIconModule,
+		MatButtonModule,
+		MatRippleModule,
+		FontAwesomeModule,
+		MatListModule,
+	],
 })
-export class AdminModule { }
+export class AdminModule {}
