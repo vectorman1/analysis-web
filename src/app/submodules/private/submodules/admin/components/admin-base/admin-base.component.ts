1b32838ab278690723176e23f9a5b7b7fabf0954
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-29 01:31:58 +0300

    add admin base component

---
+++ src/app/submodules/private/submodules/admin/components/admin-base/admin-base.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AdministrationOptionModel } from '@app/submodules/admin/models/administration-option.model';
+import {
+	faDollarSign as fasDollarSign,
+	faCoffee as fasCoffee,
+	faUsers as fasUsers,
+} from '@fortawesome/free-solid-svg-icons';
+@Component({
+	selector: 'app-admin-base',
+	templateUrl: './admin-base.component.html',
+	styleUrls: ['./admin-base.component.scss'],
+})
+export class AdminBaseComponent implements OnInit {
+	optionLists: AdministrationOptionModel[][] = [
+		[
+			{
+				matIcon: '',
+				faIcon: fasUsers,
+				isMatIcon: false,
+				title: 'Users',
+				subtitle: 'List or Create users',
+				route: './user',
+			},
+		],
+		[
+			{
+				matIcon: 'history',
+				faIcon: fasDollarSign,
+				isMatIcon: true,
+				title: 'History',
+				subtitle: 'Manage symbol histories',
+				route: './histories',
+			},
+		],
+		[
+			{
+				matIcon: '',
+				faIcon: fasDollarSign,
+				isMatIcon: false,
+				title: 'Symbols',
+				subtitle: 'Manage symbols data',
+				route: './symbols',
+			},
+		],
+	];
+
+	fasCoffee = fasCoffee;
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
---
+++ src/app/submodules/private/submodules/admin/models/administration-option.model.ts
@@ -0,0 +1,10 @@
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+
+export class AdministrationOptionModel {
+	matIcon!: string;
+	faIcon!: IconDefinition;
+	isMatIcon!: boolean;
+	title!: string;
+	subtitle!: string;
+	route!: string;
+}
