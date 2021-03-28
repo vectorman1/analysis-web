c1040ba397a6ded14c8a7fea1b14338856a9d0b2
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-29 01:33:40 +0300

    update admin user module imports

--- src/app/submodules/private/submodules/admin/submodules/user/user.module.ts
+++ src/app/submodules/private/submodules/admin/submodules/user/user.module.ts
@@ -2,16 +2,48 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { ListComponent } from './components/list/list.component';
-import { CreateComponent } from './components/create/create.component';
-import { EditComponent } from './components/edit/edit.component';
-
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UserComponent } from './user.component';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatTableModule } from '@angular/material/table';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { UserBaseComponent } from './components/user-base/user-base.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { EffectsModule } from '@ngrx/effects';
+import { UserEffects } from './effects/user.effects';
 
 @NgModule({
-  declarations: [ListComponent, CreateComponent, EditComponent],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+	declarations: [
+		UserListComponent,
+		UserCreateComponent,
+		UserEditComponent,
+		UserComponent,
+		UserBaseComponent,
+	],
+	imports: [
+		CommonModule,
+		UserRoutingModule,
+		MatCardModule,
+		FlexLayoutModule,
+		MatTableModule,
+		NgScrollbarModule,
+		MatProgressSpinnerModule,
+		MatSortModule,
+		MatIconModule,
+		MatButtonModule,
+		FontAwesomeModule,
+		MatListModule,
+		MatRippleModule,
+		EffectsModule.forFeature([UserEffects]),
+	],
 })
-export class UserModule { }
+export class UserModule {}
