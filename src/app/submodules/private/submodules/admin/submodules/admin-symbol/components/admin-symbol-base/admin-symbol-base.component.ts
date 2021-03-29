eafdc97c5a9c6888e51da84100baa731b1bdf002
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-30 00:42:14 +0300

    add symbol base copmonent

---
+++ src/app/submodules/private/submodules/admin/submodules/admin-symbol/components/admin-symbol-base/admin-symbol-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-symbol-base',
+  templateUrl: './admin-symbol-base.component.html',
+  styleUrls: ['./admin-symbol-base.component.scss']
+})
+export class AdminSymbolBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
