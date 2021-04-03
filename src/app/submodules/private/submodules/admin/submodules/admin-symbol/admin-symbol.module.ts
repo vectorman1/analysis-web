52d59fb729a2c3f9a9f3ce281f7adb4bb583c1d1
Boris Lachev <boris.lachevbg00@gmail.com>
2021-04-03 12:58:25 +0300

    update imports for admin symbol module

--- src/app/submodules/private/submodules/admin/submodules/admin-symbol/admin-symbol.module.ts
+++ src/app/submodules/private/submodules/admin/submodules/admin-symbol/admin-symbol.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AdminSymbolRoutingModule } from './admin-symbol-routing.module';
 import { AdminSymbolBaseComponent } from './components/admin-symbol-base/admin-symbol-base.component';
-
+import { FlexModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '@app/shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import { AdminSymbolEffects } from './effects/admin-symbol.effects';
 
 @NgModule({
-  declarations: [AdminSymbolBaseComponent],
-  imports: [
-    CommonModule,
-    AdminSymbolRoutingModule
-  ]
+	declarations: [AdminSymbolBaseComponent],
+	imports: [
+		CommonModule,
+		AdminSymbolRoutingModule,
+		FlexModule,
+		MatButtonModule,
+		SharedModule,
+		EffectsModule.forFeature([AdminSymbolEffects]),
+	],
 })
-export class AdminSymbolModule { }
+export class AdminSymbolModule {}
